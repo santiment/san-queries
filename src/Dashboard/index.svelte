833e8cfcb1d70767c5dd7df6695db4c8ab0b9104
DmitriVanGuard <dimavanguard@gmail.com>
2022-10-17 09:50:19 +0300

    Dashboard data load button

--- src/Dashboard/index.svelte
+++ src/Dashboard/index.svelte
@@ -4,6 +4,10 @@
   import { PanelType } from '@/types'
   import { newPanel } from '@/stores/dashboard'
   import Panel from './Panel.svelte'
+  import { mutateComputeRawClickhouseQuery } from '@/api/rawQuery'
+  import { getParametersMap } from '@/utils/parameters'
+  import { Formatter, FormatType } from '@/PanelEditor/Result/Options/format'
+  import { notifications$ } from 'san-webkit/lib/ui/Notifications'
 
   export let dashboard
   export let selectedPanel
@@ -26,15 +30,55 @@
     panels = panels.filter((v) => v !== panel)
   }
 
-  function onExecuteClick(item) {
-    console.log(item)
+  function onGetDataClick() {
+    panels.forEach((panel) => {
+      const { query, parameters } = panel.sql
+
+      return mutateComputeRawClickhouseQuery(query, getParametersMap(parameters))
+        .then((data) => {
+          const { rows, headers, dateColumns } = data
+
+          panel.__rows = rows
+          panel.__computedSql = data
+          panel.settings.columns = headers.map((title, i) => newColumn(title, i, dateColumns))
+
+          panels = panels
+        })
+        .catch(() => {
+          notifications$.show({
+            title: 'Error during data load',
+            type: 'error',
+          })
+        })
+    })
   }
 
-  function onQueryError() {}
+  // TODO: refactor. Move to utils. Same for PanelEditor/Result/index.svelte
+  function newColumn(title, i, dateColumns) {
+    const accessor = (data) => data[i]
+
+    const column = {
+      id: i,
+      title,
+      accessor,
+      format: accessor,
+      sortAccessor: accessor,
+    }
+
+    if (dateColumns.has(i)) {
+      const { id, fn } = Formatter[FormatType.DATE]
+      column.format = (data) => fn(accessor(data))
+      column.formatter = fn
+      column.formatterId = id
+      column.sortAccessor = (data) => Date.parse(data[i])
+    }
+
+    return column
+  }
 </script>
 
 <div class="row mrg-l mrg--b justify">
-  <div class="btn-1">Get data</div>
+  <button class="btn-1" on:click={onGetDataClick}>Get data</button>
 
   <div class="relative mrg-a mrg--l">
     <Tooltip on="click" position="bottom" class="$style.tooltip" bind:isOpened>
