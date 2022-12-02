ed237638d0279ba34c994728923fa905519110ca
DmitriVanGuard <dimavanguard@gmail.com>
2022-12-02 10:56:13 +0300

    Library release

--- lib/api/dashboard/index.d.ts
+++ lib/api/dashboard/index.d.ts
@@ -1,10 +1,11 @@
 export declare const queryDashboard: (id: number) => Promise<SAN.Queries.Dashboard>;
 export declare const setQueryDashboardCache: (id: number, dashboard: any) => void;
 export declare const deleteQueryDashboardCache: (id: number) => void;
-export declare const RECENT_USER_DASHBOARDS_QUERY = "{\n  getMostRecent(types: [DASHBOARD], page: 1, pageSize: 30, currentUserDataOnly: true) {\n    data {\n      dashboard {\n        id\n        title:name\n        description\n      }\n    }\n  }\n}";
+export declare const RECENT_USER_DASHBOARDS_QUERY = "{\n  currentUser {\n    dashboards {\n      id\n      title:name\n      description\n      updatedAt\n    }\n  }\n}";
 export declare type ShortDashboard = {
     id: number;
     title: string;
-    description: any;
+    description?: string;
+    updatedAt: string;
 };
 export declare const queryUserDashboards: () => Promise<ShortDashboard[]>;
