bd13ec0a6f34211014349de5c34e501fbab853d3
DmitriVanGuard <dimavanguard@gmail.com>
2023-07-24 09:41:45 +0200

    feat(left menu): Left menu layout

---
+++ src/lib/LeftMenu/index.svelte
@@ -0,0 +1,66 @@
+<script>
+  import Svg from 'webkit/ui/Svg/svelte'
+  import Search from 'webkit/ui/Search.svelte'
+</script>
+
+<aside class="column">
+  <tabs class="row gap-s mrg-l mrg--b">
+    <tab class="btn" class:active={true}><Svg id="queries" w="16" /> Data</tab>
+    <tab class="btn"><Svg id="folder" w="16" h="14" /> Work</tab>
+    <tab class="btn"><Svg id="insight" w="14" h="16" /> Use cases</tab>
+  </tabs>
+
+  <Search placeholder="Search for tables, metrics, functions" />
+
+  <div class="credits row justify">
+    Credits left: 4567
+    <button class="my-credits">My credits</button>
+  </div>
+</aside>
+
+<style lang="scss">
+  aside {
+    width: 348px;
+    background: var(--athens);
+    padding: 0 24px;
+  }
+
+  tabs {
+    border-bottom: 1px solid var(--porcelain);
+  }
+
+  tab {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    padding: 4px 8px;
+    border-radius: 0;
+    position: relative;
+  }
+
+  .active {
+    fill: var(--green);
+    color: var(--green);
+
+    &:after {
+      display: block;
+      content: '';
+      position: absolute;
+      bottom: -1px;
+      height: 1px;
+      left: 0;
+      right: 0;
+      background: var(--green);
+    }
+  }
+
+  .credits {
+    background: var(--porcelain);
+    margin: 0 -24px;
+    padding: 6px 24px;
+  }
+
+  .my-credits {
+    padding: 0 10px;
+  }
+</style>
---
+++ stories/LeftMenu/index.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/svelte'
+
+import Component from '$lib/LeftMenu/index.svelte'
+
+const meta = {
+  component: Component,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/svelte/configure/story-layout
+    layout: 'fullscreen',
+  },
+} satisfies Meta<Component>
+type Story = StoryObj<typeof meta>
+
+export default meta
+
+export const LoggedIn: Story = {
+  parameters: {
+    mockApi: () => ({
+      currentUser: {},
+    }),
+  },
+}
