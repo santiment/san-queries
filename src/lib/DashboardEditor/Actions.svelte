56aff8e9253b978a184c9c079f3a3c9ca56135c0
DmitriVanGuard <dimavanguard@gmail.com>
2023-07-27 21:20:42 +0200

    feat(dashboard editor): Basic editor layout

---
+++ src/lib/DashboardEditor/Actions.svelte
@@ -0,0 +1,50 @@
+<script>
+  import Svg from 'webkit/ui/Svg/svelte'
+</script>
+
+<actions class="row gap-l caption">
+  <button class="btn">
+    <Svg id="chart" w="16" />
+    Query
+  </button>
+  <button class="btn">
+    <Svg id="chart" w="16" />
+    Chart
+  </button>
+  <button class="btn">
+    <Svg id="image" w="16" />
+    Image
+  </button>
+  <button class="btn">
+    <Svg id="editor/title" w="16" />
+    Heading
+  </button>
+  <button class="btn">
+    <Svg id="editor/title" w="16" />
+    Text
+  </button>
+</actions>
+
+<style>
+  actions {
+    position: fixed;
+    bottom: 24px;
+    padding: 8px 24px;
+    background: #2f354d;
+    fill: #fff;
+    color: #fff;
+    border-radius: 6px;
+    align-self: center;
+  }
+
+  button {
+    gap: 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 10px;
+    padding-top: 8px;
+    --color-hover: var(--green);
+    min-width: 32px;
+  }
+</style>
---
+++ src/lib/DashboardEditor/index.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import Actions from './Actions.svelte'
+
+  let title = ''
+
+  function onBlur(e: Event) {
+    const node = e.currentTarget as HTMLElement
+    const text = (node.textContent || '').trim()
+    node.innerText = text
+  }
+</script>
+
+<main class="column">
+  <h1
+    class="h4 txt-m mrg-s mrg--b"
+    contenteditable="true"
+    placeholder="Add your title here..."
+    on:blur={onBlur}
+  >
+    {title}
+  </h1>
+
+  <p class="body-2" contenteditable="true" placeholder="Add description here..." on:blur={onBlur}>
+    {title}
+  </p>
+
+  <Actions />
+</main>
+
+<style lang="scss">
+  main {
+    padding: 24px 24px 80px;
+  }
+
+  [contenteditable] {
+    outline: none;
+
+    &:empty::before {
+      content: attr(placeholder);
+      display: block;
+      height: 100%;
+      color: var(--waterloo);
+    }
+  }
+</style>
