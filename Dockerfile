fb55ed5edd69b338ec874d40437f51f50425789b
DmitriVanGuard <dimavanguard@gmail.com>
2023-08-11 10:02:00 +0300

    chore: docker and jenkins file

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:16 AS builder
+
+ARG BACKEND_URL
+ARG API_FETCH_ORIGIN
+ARG GQL_SERVER_URL
+ARG GIT_HEAD
+ARG STAGE_SENTRY_DSN
+ARG PRODUCTION_SENTRY_DSN
+RUN GIT_HEAD=$GIT_HEAD
+
+RUN npm install -g pnpm
+
+WORKDIR /app
+
+COPY ./ /app
+
+RUN pnpm i
+RUN pnpm build
+
+FROM node:16
+
+COPY --from=builder /app/ /app/
+
+CMD [ "node", "app/build/index.js" ]
---
+++ src/routes/dashboard/new/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  // import { getCurrentUser$Ctx } from 'webkit/stores/user'
+  // import QueryHead from '$lib/QueryHead/index.svelte'
+  import DashboardEditor from '$lib/DashboardEditor/index.svelte'
+
+  // const { currentUser$ } = getCurrentUser$Ctx()
+</script>
+
+<main class="column">
+  <DashboardEditor />
+</main>
+
+<style>
+  main {
+    flex: 1;
+    padding: 0 24px;
+  }
+</style>
