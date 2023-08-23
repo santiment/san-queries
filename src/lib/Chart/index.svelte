09b32d9693bbc79aecc817eb31b5adc20a01735f
DmitriVanGuard <dimavanguard@gmail.com>
2023-08-23 10:06:31 +0300

    feat(chart): Displaying last value on axis

--- src/lib/Chart/index.svelte
+++ src/lib/Chart/index.svelte
@@ -10,22 +10,91 @@
 
   let chartNode: HTMLCanvasElement
 
+  const TEST = {
+    id: 'custom_canvas_background_color',
+    beforeDraw: (chart, args, options) => {
+      const { ctx } = chart
+      ctx.save()
+
+      // ctx.globalCompositeOperation = 'destination-over'
+
+      const BUBBLE_HEIGHT = 13
+      const BUBBLE_HALF_HEIGHT = Math.round(BUBBLE_HEIGHT / 2)
+      const BUBBLE_PADDING = 3
+      const BUBBLE_DOUBLE_PADDING = BUBBLE_PADDING + BUBBLE_PADDING
+
+      chart.data.datasets.forEach(({ yAxisID, parsing }) => {
+        const key = parsing.yAxisKey
+        const { min, max, lastValue } = options.MinMax[key]
+        const scale = chart.scales[yAxisID]
+
+        const factor = scale.height / (max - min)
+        const top = (max - lastValue) * factor + scale.top
+
+        let { left, width } = scale
+        left += BUBBLE_PADDING
+
+        ctx.fillStyle = options.color
+        ctx.fillRect(left, top - BUBBLE_HALF_HEIGHT, width - BUBBLE_DOUBLE_PADDING, BUBBLE_HEIGHT)
+
+        ctx.textBaseline = 'middle'
+        ctx.fillStyle = 'black'
+        ctx.fillText(lastValue, left + BUBBLE_PADDING, top)
+      })
+
+      ctx.restore()
+    },
+    defaults: {
+      color: 'lightGreen',
+      MinMax: {},
+    },
+  }
+
   onMount(() => {
     import('chart.js/auto').then(({ default: ChartJs }) => {
       ChartJs.defaults.font.family = 'Proxima Nova'
       ChartJs.defaults.font.size = 10
 
-      console.log(
-        { data, metrics, xAxisKey },
-        data.map((row) => row[xAxisKey]),
-      )
+      const MinMax = {} as Record<string, { min: number; max: number }>
+      const scales = {
+        x: {
+          ticks: {
+            maxTicksLimit: 10,
+          },
+        },
+      } as Record<string, any>
 
-      const scales = {} as Record<string, any>
-      metrics.slice(0, 4).forEach(({ key }) => {
-        scales[key] = { position: 'right' }
+      const axesMetrics = metrics.slice(0, 4).map(({ key }) => key)
+
+      data.forEach((row) => {
+        axesMetrics.forEach((key) => {
+          const value = row[key]
+
+          if (!MinMax[key]) MinMax[key] = { min: Infinity, max: -Infinity }
+
+          const minMax = MinMax[key]
+
+          if (value < minMax.min) minMax.min = value
+          if (value > minMax.max) minMax.max = value
+          if (Number.isFinite(value)) minMax.lastValue = value
+        })
       })
 
-      new ChartJs(chartNode, {
+      axesMetrics.forEach((key) => {
+        const { min, max } = MinMax[key]
+        scales[key] = {
+          position: 'right',
+          min,
+          max,
+          ticks: {
+            stepSize: (max - min) / 5,
+          },
+        }
+      })
+
+      ChartJs.register(TEST)
+
+      const chart = new ChartJs(chartNode, {
         type: 'line',
 
         options: {
@@ -34,7 +103,7 @@
 
           interaction: {
             intersect: false,
-            mode: 'nearest',
+            mode: 'index',
           },
 
           plugins: {
@@ -46,13 +115,12 @@
                 size: 12,
               },
             },
+            custom_canvas_background_color: {
+              MinMax,
+            },
           },
 
           scales,
-
-          parsing: {
-            xAxisKey,
-          },
         },
 
         data: {
@@ -61,6 +129,7 @@
             data,
             label: metric.title,
             yAxisID: metric.key,
+
             parsing: {
               yAxisKey: metric.key,
               xAxisKey: xAxisKey,
--- src/lib/api/query/index.ts
+++ src/lib/api/query/index.ts
@@ -1,25 +1,53 @@
 import { Universal } from 'webkit/api'
 
-const SQL = `WITH
-	'2023-01-01' AS start_date,
-	'2023-05-27' AS end_date,
-	'pepe' AS asset,
-	whales AS
-	(
-		SELECT address
-		FROM current_label_addresses
-		WHERE label_id IN (SELECT label_id FROM label_metadata WHERE (key = 'whale') AND (asset_name = asset))
-	)
-SELECT 
-	to AS address, 
-	count(*) AS buying_transaction_count, 
-	max(dt) AS last_transaction_dt
-FROM erc20_transfers
-WHERE assetRefId = (dictGet('assets_by_name', 'asset_ref_id', asset))
-AND dt >= start_date AND dt < end_date
-AND to IN (SELECT * FROM whales)
-GROUP BY to
-LIMIT 10`
+// const SQL = `WITH
+// 	'2023-01-01' AS start_date,
+// 	'2023-05-27' AS end_date,
+// 	'pepe' AS asset,
+// 	whales AS
+// 	(
+// 		SELECT address
+// 		FROM current_label_addresses
+// 		WHERE label_id IN (SELECT label_id FROM label_metadata WHERE (key = 'whale') AND (asset_name = asset))
+// 	)
+// SELECT
+// 	to AS address,
+// 	count(*) AS buying_transaction_count,
+// 	max(dt) AS last_transaction_dt
+// FROM erc20_transfers
+// WHERE assetRefId = (dictGet('assets_by_name', 'asset_ref_id', asset))
+// AND dt >= start_date AND dt < end_date
+// AND to IN (SELECT * FROM whales)
+// GROUP BY to
+// LIMIT 10`
+//
+const SQL = `SELECT
+  toStartOfMonth(dt) as month,
+  min(total_active_market_cap) as low_total_active_market_cap,
+  max(total_active_market_cap) as high_total_active_market_cap
+FROM (  
+SELECT
+    dt,
+    sum(active_market_cap) AS total_active_market_cap
+    FROM
+    (
+        SELECT
+            dt,
+            asset_id,
+            argMaxIf(value, computed_at, metric_id=dictGet('default.metrics_by_name','metric_id','daily_avg_price_usd')) AS daily_avg_price_usd,
+            argMaxIf(value, computed_at, metric_id = dictGet('default.metrics_by_name','metric_id','stack_circulation_365d')) AS stack_circulation_365d,
+            daily_avg_price_usd * stack_circulation_365d as active_market_cap
+        FROM daily_metrics_v2
+        WHERE dt>'2020-01-01' AND metric_id in (dictGet('default.metrics_by_name','metric_id','daily_avg_price_usd'),dictGet('default.metrics_by_name','metric_id','stack_circulation_365d'))
+        
+        GROUP BY asset_id, dt
+        HAVING active_market_cap > 100000 AND active_market_cap < 1000000000000
+    )
+    GROUP BY dt
+    ORDER BY dt
+)
+group by month
+`
 
 type VariablesType = {
   sql: string
