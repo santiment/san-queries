b0b95e7b4514b1079b4316cb3d9ba80175536e05
DmitriVanGuard <dimavanguard@gmail.com>
2023-09-27 16:26:02 +0800

    feat(parameters): Setting parameter title, type and value

--- src/lib/Parameter/AddParameterDialog.svelte
+++ src/lib/Parameter/AddParameterDialog.svelte
@@ -12,24 +12,43 @@
   import Control from '$lib/QueryEditor/Visualisation/Control.svelte'
 
   // const { queryEditor$ } = getQueryEditor$Ctx()
+
+  export let parameter = { title: '', value: '', type: 'Text' }
+  export let DialogCtx: SAN.Dialog.Ctx
+
+  function onAddClick() {
+    console.log(parameter)
+  }
 </script>
 
 <Dialog {...$$props} title="Add parameter">
   <div class="dialog-body column gap-l">
-    <Control name="Name" value="" placeholder="Name of the parameter" onUpdate={console.log} />
-    <Control name="Type" options={['Text', 'Number']} value="Text" onUpdate={console.log} />
+    <Control
+      name="Name"
+      value={parameter.title}
+      placeholder="Name of the parameter"
+      onUpdate={(value) => (parameter.title = value)}
+    />
+
+    <Control
+      name="Type"
+      options={['Text', 'Number']}
+      value={parameter.type}
+      onUpdate={(value) => (parameter.type = value)}
+    />
+
     <Control
       name="Value"
-      value=""
+      value={parameter.value}
       placeholder="Default value of the parameter"
-      onUpdate={console.log}
+      onUpdate={(value) => (parameter.value = value)}
     />
 
     <!-- <SQLEditor value={$queryEditor$.sql} onValueChange={onEditorValueChange} /> -->
 
     <actions class="row gap-l mrg-l mrg--t">
-      <button class="btn-1">Add</button>
-      <button class="btn-2">Cancel</button>
+      <button class="btn-1" on:click={onAddClick}>Add</button>
+      <button class="btn-2" on:click={() => DialogCtx.close()}>Cancel</button>
     </actions>
   </div>
 </Dialog>
