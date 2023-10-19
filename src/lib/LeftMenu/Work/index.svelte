e0bdffdc8b1891f7a3b0e3e13fc68445d1676555
DmitriVanGuard <dimavanguard@gmail.com>
2023-10-19 12:22:40 +0200

    feat(left menu): Workspace new folder button

--- src/lib/LeftMenu/Work/index.svelte
+++ src/lib/LeftMenu/Work/index.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
+  import Svg from 'webkit/ui/Svg/svelte'
   import MenuItem from '../MenuItem.svelte'
   import Folder from '../Folder.svelte'
 </script>
 
-<h2 class="txt-m mrg-l mrg--b">My Workspace</h2>
+<section class="row v-center justify mrg-s mrg--b">
+  <h2 class="txt-m">My Workspace</h2>
+
+  <actions class="row v-center">
+    <button class="btn-3 expl-tooltip" aria-label="New folder">
+      <Svg id="new-folder" w="12" />
+    </button>
+  </actions>
+</section>
 
 <Folder title="Let's get you started">
   {#each ['Data exploration', 'Commands', 'Functions', 'Get started dashboard'] as item, i}
@@ -14,4 +23,8 @@
 </Folder>
 
 <style lang="scss">
+  .expl-tooltip {
+    --expl-position-y: 4px;
+    --expl-right: 0;
+  }
 </style>
