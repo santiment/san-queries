7ff6d96a990999bfdcd6c94201b87e0267953d34
DmitriVanGuard <dimavanguard@gmail.com>
2023-10-20 15:57:02 +0200

    feat(left menu): Dynamic work tab structure

--- src/lib/LeftMenu/Work/index.svelte
+++ src/lib/LeftMenu/Work/index.svelte
@@ -2,25 +2,71 @@
   import Svg from 'webkit/ui/Svg/svelte'
   import MenuItem from '../MenuItem.svelte'
   import Folder from '../Folder.svelte'
+
+  const TreeItemType = {
+    FOLDER: 'FOLDER',
+    QUERY: 'QUERY',
+    DASHBOARD: 'DASHBOARD',
+  } as const
+
+  let WorkspaceTree = [
+    {
+      type: TreeItemType.FOLDER,
+      name: "Let's get you started",
+      children: [
+        {
+          type: TreeItemType.QUERY,
+          name: 'Data exploration',
+        },
+        {
+          type: TreeItemType.QUERY,
+          name: 'Commands',
+        },
+        {
+          type: TreeItemType.QUERY,
+          name: 'Functions',
+        },
+        {
+          type: TreeItemType.DASHBOARD,
+          name: 'Get started dashboard',
+        },
+      ],
+    },
+  ]
+
+  function onNewFolderClick() {
+    WorkspaceTree.push({
+      type: TreeItemType.FOLDER,
+      name: 'Untitled folder',
+      children: [],
+    })
+    WorkspaceTree = WorkspaceTree
+  }
 </script>
 
 <section class="row v-center justify mrg-s mrg--b">
   <h2 class="txt-m">My Workspace</h2>
 
   <actions class="row v-center">
-    <button class="btn-3 expl-tooltip" aria-label="New folder">
+    <button class="btn-3 expl-tooltip" aria-label="New folder" on:click={onNewFolderClick}>
       <Svg id="new-folder" w="12" />
     </button>
   </actions>
 </section>
 
-<Folder title="Let's get you started">
-  {#each ['Data exploration', 'Commands', 'Functions', 'Get started dashboard'] as item, i}
-    <MenuItem moreActions icon={i < 3 ? 'query' : 'dashboard'}>
-      {item}
-    </MenuItem>
-  {/each}
-</Folder>
+{#each WorkspaceTree as item}
+  {@const { type } = item}
+  {#if type === TreeItemType.FOLDER}
+    <Folder title={item.name}>
+      {#each item.children as item}
+        {@const { type } = item}
+        <MenuItem moreActions icon={type === TreeItemType.QUERY ? 'query' : 'dashboard'}>
+          {item.name}
+        </MenuItem>
+      {/each}
+    </Folder>
+  {/if}
+{/each}
 
 <style lang="scss">
   .expl-tooltip {
