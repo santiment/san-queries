3727c5b5ee9b2aa13ad426e426b936fa41c7944a
Vlad Keliy <vekkele@gmail.com>
2024-01-31 11:57:49 +0300

    feature(analytics) Add page view event

--- src/routes/Tracking.svelte
+++ src/routes/Tracking.svelte
@@ -5,12 +5,18 @@
   import { getCurrentUser$Ctx } from 'webkit/stores/user'
   import { updateAmplitude } from 'webkit/analytics/amplitude'
   import { startLinksListener } from 'webkit/analytics/links'
+  import { trackPageView } from 'webkit/analytics/events/general'
+  import { page } from '$app/stores'
 
   const { currentUser$ } = getCurrentUser$Ctx()
+
+  let source = ''
+  let sourceSearchParams = ''
 
   if (BROWSER) {
     startLinksListener()
     currentUser$.subscribe(setupAmplitude)
+    page.subscribe(({ url }) => trackPageChange(url))
   }
 
   function setupAmplitude(user: CurrentUserType | null) {
@@ -19,5 +25,23 @@
     const { id, username, email } = user
 
     updateAmplitude(id, username, email)
+  }
+
+  function trackPageChange(url: URL) {
+    const path = url.pathname
+    const searchParams = '?' + url.searchParams.toString()
+
+    if (source !== path) {
+      trackPageView({
+        url: path,
+        searchParams,
+
+        sourceUrl: source,
+        sourceSearchParams,
+      } as any)
+    }
+
+    source = path
+    sourceSearchParams = searchParams.toString()
   }
 </script>
