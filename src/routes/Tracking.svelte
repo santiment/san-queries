2089c20d10b266809fcffda9fe905e195e65a2c3
Vlad Keliy <vekkele@gmail.com>
2024-01-31 11:57:14 +0300

    feature(analytics) Add user properties

--- src/routes/Tracking.svelte
+++ src/routes/Tracking.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
-  import { startLinksListener } from 'webkit/analytics/links'
+  import type { CurrentUserType } from 'webkit/stores/user'
+
   import { BROWSER } from 'esm-env'
+  import { getCurrentUser$Ctx } from 'webkit/stores/user'
+  import { updateAmplitude } from 'webkit/analytics/amplitude'
+  import { startLinksListener } from 'webkit/analytics/links'
+
+  const { currentUser$ } = getCurrentUser$Ctx()
 
   if (BROWSER) {
     startLinksListener()
+    currentUser$.subscribe(setupAmplitude)
+  }
+
+  function setupAmplitude(user: CurrentUserType | null) {
+    if (!user) return
+
+    const { id, username, email } = user
+
+    updateAmplitude(id, username, email)
   }
 </script>
