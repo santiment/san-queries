1467aa4b8ef5001ed356fb6c2873bfd976e13862
DmitriVanGuard <dimavanguard@gmail.com>
2024-06-19 20:42:17 +0200

    feat(dashboard): Block layout support

--- src/lib/DashboardNext/BlockEditor/index.svelte
+++ src/lib/DashboardNext/BlockEditor/index.svelte
@@ -6,8 +6,9 @@
   import Underline from '@tiptap/extension-underline'
   import TextStyle from '@tiptap/extension-text-style'
   import Link from '@tiptap/extension-link'
+  import BlockLayout, { Column, Columns } from 'tiptap-block-layout'
 
-  // import BlockLayout, { Column, Columns } from 'tiptap-block-layout'
+  import BlockActions from './extensions/BlockActions/index.svelte'
   import BubbleMenu from './extensions/BubbleMenu/index.svelte'
 
   let { readonly = true, content = '' } = $props()
@@ -39,25 +40,88 @@
         Highlight.configure({
           multicolor: true,
         }),
+
+        Columns,
+        Column,
+        BlockLayout.configure({ dropareaColor: 'var(--droparea-color)' }),
       ],
     }}
   >
     <BubbleMenu></BubbleMenu>
+    <BlockActions></BlockActions>
   </Editor>
 </div>
 
-<style>
+<style lang="postcss">
   div :global {
     & > article {
       display: contents;
+    }
+
+    .hide {
+      @apply hidden;
     }
 
     .tiptap {
       @apply -mx-12 -mt-4 flex flex-1 flex-col p-6 px-12 pb-20 outline-none;
     }
 
-    a {
-      color: var(--green);
+    h1:not(.svelte-renderer *) {
+      @apply text-2xl;
+    }
+
+    h2:not(.svelte-renderer *) {
+      @apply text-xl;
+    }
+
+    h3:not(.svelte-renderer *) {
+      @apply text-lg;
+    }
+
+    h4:not(.svelte-renderer *) {
+      @apply text-base;
+    }
+
+    p:not(.svelte-renderer *) {
+      @apply py-0.5 text-base;
+    }
+    a:not(.svelte-renderer *) {
+      @apply text-green;
+    }
+
+    [data-type='columns'] {
+      gap: 16px;
+      display: flex;
+    }
+
+    [data-type='column'] {
+      position: relative;
+      min-width: 0;
+
+      flex: 6;
+      transition: flex-grow 180ms ease;
+
+      &:last-child > [data-type='column-resizer'] {
+        display: none;
+      }
+    }
+
+    [data-type='column-resizer'] {
+      position: absolute;
+      top: -7px;
+      bottom: -7px;
+      right: -12px;
+      width: 6px;
+      border-radius: 8px;
+      background: #c8c8c8;
+      opacity: 0.3;
+
+      cursor: col-resize;
+      z-index: 100;
+
+      &:hover {
+        opacity: 1;
+      }
     }
   }
 </style>
