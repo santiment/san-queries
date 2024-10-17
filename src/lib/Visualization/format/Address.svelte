7e6ccc2132b85e2285dd150c6bfce16beb011c12
DmitriVanGuard <dimavanguard@gmail.com>
2024-10-17 12:52:29 +0200

    feat(query): Address as a link to historical balance tool

--- src/lib/Visualization/format/Address.svelte
+++ src/lib/Visualization/format/Address.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import { copy } from 'san-webkit/lib/utils'
+  import { getAddressInfrastructure } from 'san-webkit/lib/utils/address'
   import Button from '$lib/ui/Button.svelte'
 
-  export let value: string
+  type TProps = { value: string; row?: any[] }
+  let { value, row }: TProps = $props()
 
+  const asset = $derived(row?.[1])
   let copyLabel = 'Copy'
 
   function onCopyClick() {
@@ -13,9 +16,23 @@
 </script>
 
 <wallet-address class="group flex items-center gap-1">
-  <span class="expl-tooltip" aria-label={value}>
-    {value.slice(0, 4)}...{value.slice(-4)}
-  </span>
+  {#snippet address()}
+    <span class="expl-tooltip" aria-label={value}>
+      {value.slice(0, 4)}...{value.slice(-4)}
+    </span>
+  {/snippet}
+
+  {#if getAddressInfrastructure(value)}
+    <a
+      href="https://app.santiment.net/labs/balance?address={value}{asset ? '&assets=' + asset : ''}"
+      target="_blank"
+      class="hover:text-green"
+    >
+      {@render address()}
+    </a>
+  {:else}
+    {@render address()}
+  {/if}
 
   <Button icon="copy" iconSize="12" class="invisible group-hover:visible" onclick={onCopyClick}
   ></Button>
