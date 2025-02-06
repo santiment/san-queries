b42ccfb43052264d2d389573ef46417ce0c1ad6f
DmitriVanGuard <dimavanguard@gmail.com>
2025-02-06 13:37:11 +0100

    feat(dashboard): Query column text widget markdown support

--- src/lib-next/dashboard/DocumentContent/extensions/query-text-column-block-node/ui/Text.svelte
+++ src/lib-next/dashboard/DocumentContent/extensions/query-text-column-block-node/ui/Text.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { TDashboardDataWidgetByType } from '$lib-next/dashboard/ctx/data-widgets.svelte'
   import { useDashboardSqlQueriesCtx } from '$lib-next/dashboard/ctx/dashboard-queries.svelte'
+  import { markdownToHTML } from 'san-webkit/lib/ui/Editor/markdown'
 
   type TProps = { sqlDataWidget?: TDashboardDataWidgetByType['query-widget']; column: number }
   let { sqlDataWidget, column }: TProps = $props()
@@ -20,7 +21,16 @@
 {#if !columnData || columnData.length === 0}
   Data is empty
 {:else}
-  <div class="whitespace-pre-line">
-    {columnData.slice(0, 1)}...
+  <div class="">
+    {@html markdownToHTML(columnData.slice(0, 1).toString())}
   </div>
 {/if}
+
+<style>
+  div :global {
+    ol {
+      list-style: decimal;
+      padding: 8px 0 8px 24px;
+    }
+  }
+</style>
