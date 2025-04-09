b2677def56e1c07077b74292cd0b08a200f4e020
DmitriVanGuard <dimavanguard@gmail.com>
2025-04-09 09:17:03 +0200

    feat(dashboards): testing static ssr

---
+++ src/routes/(editor)/dashboard/static/[[slug]]/+page.ts
@@ -0,0 +1,30 @@
+import { redirect } from '@sveltejs/kit'
+import { loadPageDashboard } from '../../utils.js'
+
+//export const ssr = false
+
+export async function load(event) {
+  // const { session } = await event.parent()
+  // const { customer } = session
+
+  const { slug = 'new' } = event.params
+
+  if (slug === 'new') {
+    throw redirect(302, '/dashboard/edit/new')
+  }
+
+  const [apiDashboard, dashboardDataCache] = await loadPageDashboard(event)
+
+  if (!apiDashboard) {
+    throw redirect(302, '/dashboard/edit/new')
+  }
+
+  return {
+    apiDashboard,
+    dashboardDataCache,
+
+    meta: {
+      title: `${apiDashboard.name} - Santiment Dashboard`,
+    },
+  }
+}
