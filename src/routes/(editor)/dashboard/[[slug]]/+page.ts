6737ab8306e68a655bffc5da5c9abb00e905defb
DmitriVanGuard <dimavanguard@gmail.com>
2025-04-13 21:17:20 +0300

    chore: remove page prerender

--- src/routes/(editor)/dashboard/[[slug]]/+page.ts
+++ src/routes/(editor)/dashboard/[[slug]]/+page.ts
@@ -1,12 +1,12 @@
-import type { EasyGenerator } from './$types'
+//import type { EasyGenerator } from './$types'
 
 import { redirect } from '@sveltejs/kit'
 import { loadPageDashboard } from '../utils.js'
 
-export const entries: EasyGenerator = () => {
-  return [{ slug: 'dyor-of-a-token-sentiment-social-data-and-context-1153' }]
-}
-export const prerender = true
+//export const entries: EasyGenerator = () => {
+//  return [{ slug: 'dyor-of-a-token-sentiment-social-data-and-context-1153' }]
+//}
+//export const prerender = true
 
 //export const ssr = false
 
